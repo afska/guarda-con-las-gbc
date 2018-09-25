a75e0d3efe1b3958f2883a779a6480d639dcedd7
Rodrigo Alfonso <rodri042@gmail.com>
2018-09-25 05:46:16 -0300

    Adding stars speed

--- include/ZGBMain.h
+++ include/ZGBMain.h
@@ -11,8 +11,10 @@ _SPRITE_COLOR(SPRITE_STARS, stars, 3, FRAME_32x32, 2) \
 _SPRITE_DMG(SPRITE_WORLD, world, 3, FRAME_8x8, 1) \
 SPRITE_DEF_END
 
-#define $DATA ((struct Data*) THIS->custom_data)
-
 #include "ZGBMain_Init.h"
+
+#define $DATA ((struct Data*) THIS->custom_data)
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
 
 #endif
--- src/SpriteWorld.c
+++ src/SpriteWorld.c
@@ -7,30 +7,40 @@ UINT8 bank_SPRITE_WORLD = 2;
 #include "Keys.h"
 
 static UINT8 WALK_SPEED = 3;
+static UINT8 PLAYER_X = 10;
+static UINT8 PLAYER_Y = 100;
+static UINT8 STAR1_X = 30;
+static UINT8 STAR2_X = 100;
+static UINT8 STARS_RESPAWN_MARGIN = 10;
+static UINT8 WIDTH = 160;
 
 static struct Data {
 	struct Sprite* halu;
 	struct Sprite* stars[2];
 };
 
-void Start_SPRITE_WORLD() {
-	$DATA->halu = SpriteManagerAdd(SPRITE_HALU, 10, 100);
+static void scrollBy(INT8 x);
 
-	$DATA->stars[0] = SpriteManagerAdd(SPRITE_STARS, 30, 10);
+void Start_SPRITE_WORLD() {
+	$DATA->halu = SpriteManagerAdd(SPRITE_HALU, PLAYER_X, PLAYER_Y);
+
+	$DATA->stars[0] = SpriteManagerAdd(SPRITE_STARS, STAR1_X, 10);
 	$DATA->stars[0]->current_frame = 0;
-	$DATA->stars[1] = SpriteManagerAdd(SPRITE_STARS, 100, 20);
+	$DATA->stars[1] = SpriteManagerAdd(SPRITE_STARS, STAR2_X, 20);
 	$DATA->stars[1]->current_frame = 1;
 }
 
 void Update_SPRITE_WORLD() {
-	/*if (KEY_PRESSED(J_LEFT)) scrollBy(-WALK_SPEED);
-	if (KEY_PRESSED(J_RIGHT)) scrollBy(WALK_SPEED);*/
+	if (KEY_PRESSED(J_RIGHT)) scrollBy(WALK_SPEED);
 }
 
 void Destroy_SPRITE_WORLD() {
 }
 
-/*static void scrollBy(INT8 x) {
-	TranslateSprite($DATA->stars[0], x << delta_time, 0);
-	TranslateSprite($DATA->stars[1], x << delta_time, 0);
-}*/
+static void scrollBy(INT8 x) {
+	$DATA->stars[0]->x = MIN(MAX($DATA->stars[0]->x - x, 0), WIDTH);
+	$DATA->stars[1]->x = MIN(MAX($DATA->stars[1]->x - x, 0), WIDTH);
+
+	if ($DATA->stars[0]->x <= 1) $DATA->stars[0]->x = WIDTH - STARS_RESPAWN_MARGIN;
+	if ($DATA->stars[1]->x <= 1) $DATA->stars[1]->x = WIDTH - STARS_RESPAWN_MARGIN;
+}
